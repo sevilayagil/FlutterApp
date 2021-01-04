f04298f5b7d4d2c133bf1177ad5eb258322fde04
sevilay ağıl <sevilayagil@sevilay-MacBook-Pro.local>
2021-01-04 23:02:30 +0300

    question.dart added

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() => runApp(MyCoolApp());
 
-class MyCoolApp extends StatelessWidget {
-  var questionIndex = 0;
-  void answerQuestions() {
-    questionIndex = questionIndex + 1;
-    print(questionIndex);
+class MyCoolApp extends StatefulWidget {
+  @override
+  _MyCoolAppState createState() => _MyCoolAppState();
+}
+
+class _MyCoolAppState extends State<MyCoolApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestions() {
+    _questionIndex = _questionIndex + 1;
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = ['Question 1', 'Question 2'];
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Sevo\'s First App',
-          ),
+    var scaffold = Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        title: Text(
+          'Sevo\s First App',
         ),
-        body: Column(
-          children: [
-            Text(
-              questions[questionIndex],
+      ),
+      body: Column(
+        children: [
+          Question(
+            questions[_questionIndex],
+          ),
+          RaisedButton(
+            onPressed: _answerQuestions,
+            child: Text(
+              'Answer 1',
               style: TextStyle(
                   color: Colors.purple,
                   fontWeight: FontWeight.w500,
@@ -30,27 +42,24 @@ class MyCoolApp extends StatelessWidget {
                   fontStyle: FontStyle.italic,
                   fontSize: 25),
             ),
-            RaisedButton(
-              onPressed: answerQuestions,
-              child: Text(
-                'Answer 1',
-              ),
-              shape: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-            ),
-            RaisedButton(
-              onPressed: answerQuestions,
-              child: Text('Answer 2'),
-              shape: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15))),
-            ),
-            FloatingActionButton(
-              onPressed: answerQuestions,
-              child: Icon(Icons.favorite),
-            )
-          ],
-        ),
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+          ),
+          RaisedButton(
+            onPressed: _answerQuestions,
+            child: Text('Answer 2'),
+            shape: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+          ),
+          FloatingActionButton(
+            onPressed: _answerQuestions,
+            child: Icon(Icons.favorite),
+          )
+        ],
       ),
+    );
+    return MaterialApp(
+      home: scaffold,
     );
   }
 }
